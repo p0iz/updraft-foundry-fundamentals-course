d419dd33c770386895f5816621816e51eb28c82f
Tommi Martela <tommi.martela@gmail.com>
2025-08-08 14:35:25 +0300

    Refactor price feed interface instantiation

--- FundMe/test/FundMeTest.t.sol
+++ FundMe/test/FundMeTest.t.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe dfm = new DeployFundMe();
+        fundMe = dfm.run();
     }
 
     function testMinimumDollarIsFive() public view {
@@ -16,7 +17,7 @@ contract FundMeTest is Test {
     }
     
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testGetVersionIsCorrect() public view {
